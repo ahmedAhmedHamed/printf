51c3ac38bebdd648e9fd3d81e1a619f9d68aa930
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-26 12:18:54 -0700

    s

--- my_printf.c
+++ my_printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strlen - helo
  * @s: number
@@ -19,51 +18,46 @@ return (len);
  * @format: format to print
  * @...: strings to print
  * Return: the number of characters printed
- *
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int charactersprinted = 0;
-    int formatlength = _strlen(format);
+va_list args;
+int i = 0;
+int charactersprinted = 0;
+int formatlength = _strlen(format);
 
-    va_start(args, format);/*start of args loop*/
-    for (i = 0; i < formatlength; i++)
-    {
-        if (format[i] != '%')/*if character is not an identifier, print it*/
-        {
-            write(1, &format[i], 1);
-            charactersprinted++;
-            continue;
-        }
-
-        if (format[i + 1] != 's' && format[i + 1] != 'c')/*if character after % is not s or c then print %*/
-        {
-            write(1, &format[i], 1);
-            charactersprinted++;
-            if (format[i + 1] == '%')/*if % has % after it increment i*/
-                i++;
-            continue;
-        }
-
-        if (format[i + 1] == 's')/*identifier is string, print the string*/
-        {
-            char *str = va_arg(args, char );
-                       if (str == 0)
-                str = "(null)"; 
-            charactersprinted += _strlen(str);
-            write(1, str, _strlen(str));
-        }
-
-        else if (format[i + 1] == 'c')/*identifier is char, print the character*/
-        {
-            int singlechar = va_arg(args, int);
-            charactersprinted++;
-            write(1, &singlechar, 1);
-        }
-        i++;/*i needs to be incremented if an identifier is used in order to not print the identification character*/
-    }
-    va_end(args);/*end of args loop*/
-    return (charactersprinted);
+va_start(args, format);
+for (i = 0; i < formatlength; i++)
+{
+if (format[i] != '%')
+{
+write(1, &format[i], 1);
+charactersprinted++;
+continue;
+}
+if (format[i] != 's' && format[i] != 'c')
+{
+write(1, &format[i], 1);
+charactersprinted++;
+if (format[i + 1] == '%')
+i++;
+continue;
+}
+if (format[i + 1] == 's')
+{
+char *str = va_arg(args, char *);
+charactersprinted += _strlen(str);
+write(1, str, _strlen(str));
+}
+else if (format[i + 1] == 'c')
+{
+int singlechar = va_arg(args, int);
+if (singlechar != '\0')
+charactersprinted++;
+write(1, &singlechar, 1);
+}
+i++;
+}
+va_end(args);
+return (charactersprinted);
 }
