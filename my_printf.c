3af54365ad55d029981cd728a0a2d32d7fff87c9
ahmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-03-26 12:24:39 -0700

    s

--- my_printf.c
+++ my_printf.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
-
 /**
  * _strlen - helo
  * @s: number
@@ -16,16 +13,6 @@ len++;
 return (len);
 }
 
-int _isDigit(int digit)
-{
-    return(digit >= 48 && digit <= 57);
-}
-
-int isIdentifier(const char *format, int i)
-{
-    return (format[i + 1] != 's' && format[i + 1] != 'c');
-}
-
 /**
  * _printf - my own printf - this one can't do anything other than %s and %c
  * @format: format to print
@@ -35,46 +22,47 @@ int isIdentifier(const char *format, int i)
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0;
-int charactersprinted = 0;
-int formatlength = _strlen(format);
-va_start(args, format);/*start of args loop*/
-for (i = 0; i < formatlength; i++)
-{
-if (format[i] != '%')/*if character is not an identifier, print it*/
-{
-write(1, &format[i], 1);
-charactersprinted++;
-continue;
-}
+    va_list args;
+    int i = 0;
+    int charactersprinted = 0;
+    int formatlength = _strlen(format);
 
-if (isIdentifier(format, i))/*if character after % is not s or c then print %*/
-{
-write(1, &format[i], 1);
-charactersprinted++;
-if (format[i + 1] == '%')/*if % has % after it increment i*/
-i++;
-continue;
-}
+    va_start(args, format);/*start of args loop*/
+    for (i = 0; i < formatlength; i++)
+    {
+        if (format[i] != '%')/*if character is not an identifier, print it*/
+        {
+            write(1, &format[i], 1);
+            charactersprinted++;
+            continue;
+        }
 
-if (format[i + 1] == 's')/*identifier is string, print the string*/
-{
-char *str = va_arg(args, char *);
-if (str == 0)
-str = "(null)";
-charactersprinted += _strlen(str);
-write(1, str, _strlen(str));
-}
+        if (format[i + 1] != 's' && format[i + 1] != 'c')/*if character after % is not s or c then print %*/
+        {
+            write(1, &format[i], 1);
+            charactersprinted++;
+            if (format[i + 1] == '%')/*if % has % after it increment i*/
+                i++;
+            continue;
+        }
 
-else if (format[i + 1] == 'c')/*identifier is char, print the character*/
-{
-int singlechar = va_arg(args, int);
-charactersprinted++;
-write(1, &singlechar, 1);
-}
-i++;/*i needs to be incremented if an identifier is used in order to not print the identification character*/
-}
-va_end(args);/*end of args loop*/
-return (charactersprinted);
+        if (format[i + 1] == 's')/*identifier is string, print the string*/
+        {
+            char *str = va_arg(args, char *);
+            if (str == 0)
+                str = "(null)";
+            charactersprinted += _strlen(str);
+            write(1, str, _strlen(str));
+        }
+
+        else if (format[i + 1] == 'c')/*identifier is char, print the character*/
+        {
+            int singlechar = va_arg(args, int);
+            charactersprinted++;
+            write(1, &singlechar, 1);
+        }
+        i++;/*i needs to be incremented if an identifier is used in order to not print the identification character*/
+    }
+    va_end(args);/*end of args loop*/
+    return (charactersprinted);
 }
